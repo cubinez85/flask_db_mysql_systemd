8aee5a84191cc4923a5b46f5feb8874def5f3d39
cubinez85 <cubinez85@mail.ru>
2024-03-30 18:26:22 +0300

    insert_and_display_db

--- myproject/myproject.py
+++ myproject/myproject.py
@@ -1,29 +1,39 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+
+
 app = Flask(__name__)
- 
- 
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'cubinez85'
 app.config['MYSQL_PASSWORD'] = '123'
 app.config['MYSQL_DB'] = 'MyDB'
- 
+
 mysql = MySQL(app)
- 
- 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
     if request.method == "POST":
         details = request.form
         firstName = details['fname']
         lastName = details['lname']
+        position = details['position']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        cur.execute("INSERT INTO MyUsers(firstName, lastName, position) VALUES (%s, %s, %s)", (firstName, lastName, position))
         mysql.connection.commit()
         cur.close()
         return 'success'
     return render_template('index.html')
- 
- 
+
+@app.route('/display')
+def display():
+    cursor = mysql.connection.cursor()
+    cursor.execute("select * from MyUsers")
+    output = cursor.fetchall()
+    res = str(output).strip('[]')
+
+    return res
+
+
 if __name__ == '__main__':
     app.run()
